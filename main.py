14a55583cfbeb14a95e8fedb0bdc45f0d8a205d8
olga_charna <53872698+Olgittaa@users.noreply.github.com>
2023-01-03 20:07:26 +0100

    requests

--- main.py
+++ main.py
@@ -2,26 +2,32 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.config import Config
-import sqlite3
+import requests
+import json
 import datetime
-
-con = sqlite3.connect('identifier.sqlite')
-cur = con.cursor()
-cur.execute('SELECT * from test')
+import mysql.connector
 
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '600')
-
 Builder.load_file('frontend.kv')
+url = 'https://test-bd0b8-default-rtdb.europe-west1.firebasedatabase.app/'
 
+result = requests.get(url + '.json')
+data = json.loads(result.content.decode())
+print(data)
 
 class MainScreen(Screen):
 
     def insert(self):
-        cur.execute(f'INSERT INTO test (date) VALUES (\'{datetime.datetime.now()}\');')
-        con.commit()
-        cur.execute('SELECT * from test order by id desc limit 1')
-        self.ids.time_label.text = cur.fetchone()[1]
+        result = requests.get(url + 'test.json')
+        data_count = len(json.loads(result.content.decode()))
+
+        requests.patch(url+'test.json', data='{"'+str(data_count)+'": "'+str(datetime.datetime.now())+'"}')
+
+        result = requests.get(url + 'test.json')
+
+        all_data = json.loads(result.content.decode())
+        self.ids.time_label.text = str(all_data[-1])
 
 
 class RootWidget(ScreenManager):
